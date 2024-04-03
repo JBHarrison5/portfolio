7507262428c7cbbf39059813457107659b380ef0
John Harrison <john.harrison147@virginmedia.com>
2024-04-03 20:44:07 +0100

    added working email

---
+++ contact.js
@@ -0,0 +1,27 @@
+function sendMail() {
+    let params = {
+        firstName: document.querySelector('#firstName').value,
+        lastName: document.querySelector('#lastName').value,
+        email: document.querySelector('#email').value,
+        message: document.querySelector('#message').value
+    }
+    const serviceID = "service_i7ctr0k";
+    const templateID = "template_ycki47a";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.querySelector('#firstName').value = "";
+                document.querySelector('#firstName').value = "";
+                document.querySelector('#firstName').value = "";
+                document.querySelector('#firstName').value = "";
+                console.log(res);
+                alert("Thank you for your message");
+            })
+        .catch((err) => console.log(err));
+}
+
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', () => {
+    sendMail();
+})
